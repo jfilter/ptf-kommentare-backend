3f61ddeff09d26aad84506d3fb6e0a66cefccb6c
Johannes Filter <hi@jfilter.de>
2019-10-12 20:30:13 +0200

    include query token

--- app.py
+++ app.py
@@ -19,12 +19,13 @@ vecs = {}
 for m in Path(data_dir).glob("*.model"):
     vecs[m.stem] = KeyedVectors.load(str(m), mmap="r")
 
+
 @app.route("/<vec_name>")
 def index(vec_name):
     q, n = request.args.get("q"), request.args.get("n", 10, type=int)
     v = vecs[vec_name]
     results = v.most_similar(q, topn=n)
-    tokens, _ = list(zip(*results))
+    tokens, _ = [q] + list(zip(*results))
     vectors = [v[t] for t in tokens]
     vectors = PCA(n_components=2).fit_transform(vectors)
     vectors = MinMaxScaler((-1, 1)).fit_transform(vectors)
